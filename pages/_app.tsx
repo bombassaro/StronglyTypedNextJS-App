d86044f19082e339cd42004d6527284a1326dbb6
Fernando <fernando.bombassaro@gmail.com>
2021-02-14 01:15:05 -0300

    config typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import App from 'next/app';
+import React from 'react';
+
+export default ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+)
---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+export default () => (
+  <b>Hello World!</b>
+)
